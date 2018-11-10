d5985bb36fd7253bb7dd8c8c2512b4100b1bfe51
Vechx007 <44900134+Vechx007@users.noreply.github.com>
2018-11-10 21:54:29 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
-﻿const Discord = require("discord.js");
+const Discord = require("discord.js");
 const client = new Discord.Client();
 const client2 = new Discord.Client();
-
+ 
 client.on('ready', () => {
    console.log(`----------------`);
    console.log(`Credit Farmm - Script By : Vechx `);
@@ -11,85 +11,78 @@ client.on('ready', () => {
    console.log(`Loadinng..`);
    console.log(`Loadinng...`);
    console.log(`This Bots Online ' `);
-   console.log(`----------------`);
+  console.log(`----------------`);
 });
-
-
-module.exports.run = async (bot, message, args) => [
-     //!say Hi!
-	 //Hi
-	 if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("No");
-	 let botmessage= args.join(" ");
-	 message.delete().catch();
-	 message.channel.send(botmessage);
-	 }
-	 
-	 module.exports.help = {
-		 name: "say"
-	 } 
-
+ 
+ 
 client.on('message', message => {
-    if(message.content === '-مبلغ'){
-        message.channel.send('#credits')
-    }
+   if(message.content === '-راتب'){
+       message.channel.send('#daily')
+   }
 });
-
+ 
 client.on('message', message => {
-    if(message.content === '-ريب'){
-        message.channel.send('#credits')
-    }
+   if(message.content === '-مبلغ'){
+       message.channel.send('#credits')
+   }
 });
-
+ 
+client.on('message', message => {
+   if(message.content === '-ريب'){
+       message.channel.send('#credits')
+   }
+});
+ 
 client2.on('message', message => {
-    if(message.content === '-راتب'){
-        message.channel.send('#daily')
-    }
+   if(message.content === '-راتب'){
+       message.channel.send('#daily')
+   }
 });
-
+ 
 client2.on('message', message => {
-    if(message.content === '-مبلغ'){
-        message.channel.send('#credits')
-    }
+   if(message.content === '-مبلغ'){
+       message.channel.send('#credits')
+   }
 });
-
+ 
 client2.on('message', message => {
-    if(message.content === '-ريب'){
-        message.channel.send('#credits')
-    }
+   if(message.content === '-ريب'){
+       message.channel.send('#credits')
+   }
 });
-
-
-
+ 
+ 
+ 
 client.on('message', message => { // لا تغير شئ عشان ما تخرب الدنيا
 if (message.content === '!spam') {
-      let count = 0;
-      let ecount = 0;
-      for(let x = 0; x < 90000; x++) {
-        message.channel.send(`**Credit Spam , Frame , Credit By Kahrba , Farm Credit By Kahrba, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
-          .then(m => {
-            count++;
-          })
-          
-        }
-      }
+     let count = 0;
+     let ecount = 0;
+     for(let x = 0; x < 90000; x++) {
+       message.channel.send(`**Credit Spam , Frame , Credit By Vechx , Farm Credit By Vechx, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
+         .then(m => {
+           count++;
+         })
+         
+       }
+     }
 });
-
+ 
 client2.on('message', message => { // لا تغير شئ عشان ما تخرب الدنيا
 if (message.content === '!spam') {
-      let count = 0;
-      let ecount = 0;
-      for(let x = 0; x < 90000; x++) {
-        message.channel.send(`**Credit Spam , Frame , Credit By Kahrba , Farm Credit By Kahrba, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
-          .then(m => {
-            count++;
-          })
-          
-        }
-      }
+     let count = 0;
+     let ecount = 0;
+     for(let x = 0; x < 90000; x++) {
+       message.channel.send(`**Credit Spam , Frame , Credit By Vechx , Farm Credit By Vechx, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
+         .then(m => {
+           count++;
+         })
+         
+       }
+     }
 });
-
-
-
-
+ 
+ 
+ 
+ 
 client.login(process.env.TOKEN);// لا تغير فيها شيء
 client2.login(process.env.TOKEN2);// لا تغير فيها شيء
